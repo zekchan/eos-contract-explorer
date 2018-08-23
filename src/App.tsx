44f4ad5515bf3f4fca176cb56397c89d6c634706
Ilya Groshev <groshev.i.d@gmail.com>
2018-08-23 23:15:56 +0300

    Tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
+import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import * as React from 'react';
 import ContractInput from './components/ContractInput';
 import Header from './components/Header';
 import { detectScatter } from './Scatter';
-
-const styles = (theme: Theme) => createStyles({
+const theme = createMuiTheme({});
+const styles = () => createStyles({
   content: {
     marginTop: 50,
   },
 })
+
 class App extends React.Component<WithStyles<typeof styles>> {
   public componentDidMount() {
     detectScatter()
   }
   public render() {
     return (
-      <React.Fragment>
+      <MuiThemeProvider theme={theme}>
         <Header />
         <div className={this.props.classes.content}>
           <ContractInput />
         </div>
-      </React.Fragment>
+      </MuiThemeProvider>
     )
   }
 }
--- src/components/ContractInfo.tsx
+++ src/components/ContractInfo.tsx
@@ -1,7 +1,9 @@
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import AppBar from '@material-ui/core/AppBar';
+// import Card from '@material-ui/core/Card';
+// import CardContent from '@material-ui/core/CardContent';
 import LinearProgress from '@material-ui/core/LinearProgress';
-import Typography from '@material-ui/core/Typography';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
 import { abi_def, getAbiResult } from "eosjs-api";
 import * as React from "react"
 import eos from "../eosService"
@@ -16,23 +18,18 @@ interface IState {
     loading: boolean,
     abi: abi_def | null,
     error: string | null,
+    tab: 0 | 1 | 2,
 }
 export default class ContractInfo extends React.Component<IProps, IState> {
     public state: IState = {
         loading: true,
         abi: null,
         error: null,
+        tab: 0
     }
 
     public componentDidMount() {
         eos.getAbi(this.props.contractName)
-            .catch((e) => {
-                this.setState({
-                    loading: false,
-                    error: e.message,
-                    abi: null,
-                })
-            })
             .then(({ abi }: getAbiResult) => {
                 // tslint:disable-next-line:no-console
                 console.log(abi)
@@ -47,44 +44,43 @@ export default class ContractInfo extends React.Component<IProps, IState> {
         if (this.state.loading) {
             return <LinearProgress />
         }
-        if (this.state.error) {
-            return <div style={{ color: "red" }}>Ошибка: {this.state.error}</div>
-        }
         const { ricardian_clauses, tables, actions, structs } = this.state.abi as abi_def
         const structsMap = structs.reduce((acc, struct) => Object.assign(acc, { [struct.name]: struct }), {})
-        return <Card>
-            <CardContent>
-                <Typography variant="headline">
-                    {this.props.contractName}
-                </Typography>
-                <Typography variant='subheading'>Tables:</Typography>
-                {tables && tables.map(
-                    table =>
-                        <ContractTable
-                            scopeName={this.props.scopeName}
-                            key={table.name}
-                            contractName={this.props.contractName}
-                            table={table}
-                            structs={structsMap}
-                        />
-                )}
-                <Typography variant='subheading'>Ricardian Clauses</Typography>
-                {
-                    ricardian_clauses
-                        .sort(({ body }) => body.length ? -1 : 1)
-                        .map(({ id, body }) =>
-                            <RicardianText name={id} body={body} key={id} />
-                        )
-                }
-                <Typography variant='subheading'>Actions</Typography>
-                {
-                    actions
-                        .sort(({ ricardian_contract }) => ricardian_contract.length ? -1 : 1)
-                        .map(({ name, ricardian_contract }) =>
-                            <RicardianText name={name} body={ricardian_contract} key={name} />
-                        )
-                }
-            </CardContent>
-        </Card>
+        return <React.Fragment>
+            <AppBar position="static">
+                <Tabs value={this.state.tab} onChange={this.handleTab}>
+                    <Tab label="Tables" />
+                    <Tab label="Ricardian Clauses" />
+                    <Tab label="Actions" />
+                </Tabs>
+            </AppBar>
+            {this.state.tab === 0 && tables && tables.map(
+                table =>
+                    <ContractTable
+                        scopeName={this.props.scopeName}
+                        key={table.name}
+                        contractName={this.props.contractName}
+                        table={table}
+                        structs={structsMap}
+                    />
+            )}
+            {
+                this.state.tab === 1 && ricardian_clauses
+                    .sort(({ body }) => body.length ? -1 : 1)
+                    .map(({ id, body }) =>
+                        <RicardianText name={id} body={body} key={id} />
+                    )
+            }
+            {
+                this.state.tab === 2 && actions
+                    .sort(({ ricardian_contract }) => ricardian_contract.length ? -1 : 1)
+                    .map(({ name, ricardian_contract }) =>
+                        <RicardianText name={name} body={ricardian_contract} key={name} />
+                    )
+            }
+        </React.Fragment>
+    }
+    private handleTab = (e: React.MouseEvent, tab: 0 | 1 | 2) => {
+        this.setState({ tab })
     }
 }
